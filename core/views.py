1fe0d8d0f290937bfb1d50eec7465239db23f487
Case Gibson <t.barbato78@gmail.com>
2023-05-02 20:21:38 +0200

    add: create new game with VueJS

--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.views.decorators.csrf import csrf_exempt
-from django.views.generic import TemplateView, FormView
+from django.views.generic import TemplateView, FormView, ListView, CreateView
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -9,7 +9,9 @@ from django.db.models import Value, Q
 from django.http import JsonResponse
 from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
+import random
 
+import settings
 from core.backend.middlewares import (
     JsonableResponseMixin,
     AuthRequiredMiddleware
@@ -29,7 +31,7 @@ from .models import (
 
 from .forms import (
     LoginForm,
-    RegisterForm
+    RegisterForm, CreateGameForm
 )
 
 
@@ -102,14 +104,79 @@ class LoginAjaxView(LoginView, JsonableResponseMixin, SuccessMessageMixin):
         return JsonResponse(form.errors, status=200)
 
 
-@method_decorator(AuthRequiredMiddleware, name="dispatch")
-class DisplayDashboardView(TemplateView, LoginRequiredMixin):
+class DisplayDashboardView(LoginRequiredMixin, ListView):
     template_name = "display/dashboard.html"
-    login_url = reverse_lazy('index_view')
+    login_url = settings.LOGIN_URL
+    model = User
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        test = User.objects.select_related('ingameplayer').filter(id=self.request.user.id)
-        print(test)
         context["max_game_per_user"] = 7
+        context["game_list_count"] = 0
+        context["CreateGameForm"] = CreateGameForm
+        context["game_list"] = Game.objects.filter(owner_uuid_id=self.request.user.id)
 
+        return context
+
+class CreateNewGameAjaxView(LoginRequiredMixin, JsonableResponseMixin, FormView):
+    template_name = "display/dashboard.html"
+    login_url = settings.LOGIN_URL
+    model = Game
+    form_class = CreateGameForm
+    success_url = reverse_lazy("dashboard_view")
+
+    def post(self, request, *args, **kwargs):
+        if Game.objects.filter(owner_uuid=self.request.user.id).count() < 7:
+            name = "sans noms" if self.request.POST.get('game_name') is None else self.request.POST.get('game_name')
+            if len(name) >= 4 and len(name) <= 16:
+                user_id = self.request.user.id
+                game_invite_code = f'{name}#{random.randint(99, 9999)}'
+                game = Game(
+                    name=name,
+                    game_invite_code=game_invite_code,
+                    owner_uuid_id=user_id
+                )
+                game.save()
+                game_id = Game.objects.filter(
+                    name=name,
+                    game_invite_code=game_invite_code,
+                    owner_uuid_id=user_id
+                ).values('id')
+
+                player = Ingameplayer(
+                    game_id=game_id,
+                    player_id=user_id,
+                    owner_uuid_id=user_id
+                )
+                player.save()
+
+                for entry in Cards.objects.values('id', 'name'):
+                    Ingamecards(
+                        card_id=entry['id'],
+                        current_state="PIOCHE" if entry['name'] != 'dos' else 'SPECIAL',
+                        game_id=game_id
+                    ).save()
+
+                game_list_count = Ingameplayer.objects.filter(Q(player_id=user_id)|Q(owner_uuid_id=user_id)).count()
+                response_data = {
+                    'success': True,
+                    'game_list_count': game_list_count,
+                    'game_data': {'name': game.name, 'id': game.id,
+                                  'owner_uuid_id': user_id }
+                }
+            else:
+                response_data = {'error': 'Le nom de votre partie doit se située entre 4 et 16 caractères.'}
+        else:
+            response_data = {'error': 'Nombre de parties crées en simultané dépassé, 7 maximum.'}
+        return JsonResponse(response_data)
+
+
+class UpdateGameListAjaxView(LoginRequiredMixin, JsonableResponseMixin, FormView):
+    template_name = "display/dashboard.html"
+    login_url = settings.LOGIN_URL
+    model = Game
+
+    def post(self, request, *args, **kwargs):
+        game_list = Game.objects.filter(owner_uuid_id=self.request.user.id)
+        print(game_list)
+        return JsonResponse(game_list, safe=False)
